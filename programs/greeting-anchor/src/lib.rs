da6d1733cc5c201e465787166cc1f54a659f5990
LFSCamargo <luizepauloxd@gmail.com>
2022-12-26 19:56:25 -0300

    feat(greeting-contract): adds greeting contract to anchor

--- programs/greeting-anchor/src/lib.rs
+++ programs/greeting-anchor/src/lib.rs
@@ -4,12 +4,28 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod greeting_anchor {
-    use super::*;
+  use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+  pub fn set_message(ctx: Context<SetMessage>, message: String) -> Result<()> {
+    let greeting_account = &mut ctx.accounts.greeting;
+    greeting_account.greeting = message;
+    msg!("Greeting updated, {}", greeting_account.greeting);
+    Ok(())
+  }
+}
+
+#[account]
+struct GreetingAccount {
+  pub greeting: String,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SetMessage<'info> {
+  #[account(init, payer = user, space = 8 + 32)]
+  greeting: Account<'info, GreetingAccount>,
+
+  #[account(mut)]
+  pub user: Signer<'info>,
+
+  pub system_program: Program<'info, System>,
+}
--- tests/greeting-anchor.ts
+++ tests/greeting-anchor.ts
@@ -1,16 +1,27 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { GreetingAnchor } from "../target/types/greeting_anchor";
+import { GreetingAnchor, IDL } from "../target/types/greeting_anchor";
 
 describe("greeting-anchor", () => {
+  const provider = anchor.AnchorProvider.env();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.GreetingAnchor as Program<GreetingAnchor>;
+  const program: Program<GreetingAnchor> = anchor.workspace.GreetingAnchor;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("should execute the function call without any issues", async () => {
+    const greetingAccount = anchor.web3.Keypair.generate();
+
+    const tx = await program.rpc.setMessage("Hello World", {
+      accounts: {
+        greeting: greetingAccount.publicKey,
+        user: provider.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      options: {
+        commitment: "confirmed",
+      },
+      signers: [greetingAccount],
+    });
   });
 });
